84723602139ade136bc22124b99bec9f43ab62f9
Ryan Helsing <ryanhelsing@gmail.com>
2015-02-11 15:23:27 -0500

    kinda hacky but works

--- pg_scraper.rb
+++ pg_scraper.rb
@@ -16,7 +16,11 @@ require 'uri'
 
 @essay_list.each do |url|
   doc = Nokogiri::HTML(open(url))
-  @chapter_content = doc.css('td')[2] #html formatted
+  @chapter_content = doc.css('td')[2].css('td')[0] #html formatted <td width="435">
   @chapter_content = @chapter_content.to_s.gsub("\n"," ")
+  @chapter_content = @chapter_content.to_s.gsub('<td width="435">','')
+  @chapter_content = @chapter_content.to_s.gsub('<td>','')
+  @chapter_content = @chapter_content.to_s.gsub('</td>','')
+  @chapter_content = @chapter_content.to_s.gsub('alt="','alt="">')
   puts @chapter_content
 end
