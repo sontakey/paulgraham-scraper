94cb42c731b604d8b768b548433121707c53da66
Sameer Sontakey <sameer@bridg.com>
2015-11-09 00:13:52 -0800

    Using rss feed

--- pg_scraper.rb
+++ pg_scraper.rb
@@ -1,21 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 require 'uri'
+require 'simple-rss'
 
-@essay_list = ["http://paulgraham.com/hp.html",
-  "http://paulgraham.com/wealth.html",
-  "http://paulgraham.com/start.html",
-  "http://paulgraham.com/hiring.html",
-  "http://paulgraham.com/opensource.html",
-  "http://paulgraham.com/notnot.html",
-  "http://paulgraham.com/startupmistakes.html",
-  "http://paulgraham.com/head.html",
-  "http://paulgraham.com/cities.html",
-  "http://paulgraham.com/startuplessons.html",
-  "http://paulgraham.com/relres.html"]
-
-@essay_list.each do |url|
-  doc = Nokogiri::HTML(open(url))
+rss = SimpleRSS.parse open("http://www.aaronsw.com/2002/feeds/pgessays.rss")
+rss.items.each do |item|
+  doc = Nokogiri::HTML(open(item.link))
   @chapter_content = doc.css('td')[2].css('td')[0] #html formatted <td width="435">
   @chapter_content = @chapter_content.to_s.gsub("\n"," ")
   @chapter_content = @chapter_content.to_s.gsub('<td width="435">','')
